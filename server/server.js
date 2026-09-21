e842006664ccd2cbbb42e5dbf91e9abf1de78d46
Zeeshan Razzaq <zeeshan66780@gmail.com>
2026-09-21 21:18:15 +0500

    comment

--- server/server.js
+++ server/server.js
@@ -40,16 +40,16 @@ app.use(
     credentials: true,
   }),
 );
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin); // dynamically set origin
-  }
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+// app.use((req, res, next) => {
+//   const origin = req.headers.origin;
+//   if (allowedOrigins.includes(origin)) {
+//     res.header("Access-Control-Allow-Origin", origin); // dynamically set origin
+//   }
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
 
 app.get("/", (req, res) => res.send("API is working!"));
 app.use("/api/user", userRouter);
