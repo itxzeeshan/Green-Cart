6323cfb95802356be507ca6e150fb95345c89ed1
Zeeshan Razzaq <zeeshan66780@gmail.com>
2026-09-21 21:33:13 +0500

    comment

--- server/configs/cloudinary.js
+++ server/configs/cloudinary.js
@@ -3,10 +3,10 @@ import { v2 as cloudinary } from "cloudinary";
 const connectCloudinary = async () => {
   console.log("Cloud Name:", process.env.CLOUDINARY_CLOUD_NAME);
 
-  cloudinary.config({
+  console.log("Cloudinary config check:", {
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+    api_key: process.env.CLOUDINARY_API_KEY ? "YES" : "NO",
+    api_secret: process.env.CLOUDINARY_API_SECRET ? "YES" : "NO",
   });
 };
 
